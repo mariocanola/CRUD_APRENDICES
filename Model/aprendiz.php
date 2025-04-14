63a54d61d32f992bc9b59598d6816020eb213653
mariocanola <macanola0@soy.sena.edu.co>
2025-04-13 22:11:52 -0500

    se implemento una nueva funcion la cual es para asignar el rol a la persona

--- Model/aprendiz.php
+++ Model/aprendiz.php
@@ -10,13 +10,6 @@ class Aprendices
         $this->db = (new Database())->connect();
     }
 
-    public function ObtenerPersonas()
-    {
-        $sql = "SELECT * FROM persona";
-        $result = $this->db->query($sql);
-        return $result->fetchAll(PDO::FETCH_ASSOC);
-    }
-
     public function obtenerPersonaConID($id_persona)
     {
         $sql = "SELECT * FROM persona WHERE id_persona = :id_id_persona";
@@ -69,4 +62,27 @@ class Aprendices
         return $stmt->execute();
     }
 
+    public function obtenerAprendices() 
+    {
+        $sql = "
+            SELECT 
+                p.id_persona, 
+                CONCAT(p.primer_nombre, ' ', IFNULL(p.segundo_nombre, ''), ' ', p.primer_apellido, ' ', IFNULL(p.segundo_apellido, '')) AS nombre_completo, 
+                pr.nombre AS programa 
+            FROM persona p 
+            JOIN aprendiz a ON p.id_persona = a.id_persona 
+            JOIN aprendiz_programa ap ON a.id_aprendiz = ap.id_aprendiz 
+            JOIN programa pr ON ap.id_programa = pr.id_programa;
+        ";
+
+        $stmt = $this->db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function obtenerRoles() 
+    {
+        $sql = "SELECT id_rol, nombre FROM rol ORDER BY nombre ASC";
+        $stmt = $this->db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
